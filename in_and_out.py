637f712238315269c48016b8b554f5ec8bc05307
Neltharion59 <lukawh5959@gmail.com>
2025-04-17 12:04:59 +0200

    Robust file read/write

--- in_and_out.py
+++ in_and_out.py
@@ -1,7 +1,22 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from utilities import file_exists
+
+
 def provide_use_cases():
-    for i in range(12):
-        file_name = 'UseCase{0}'.format(i)
-        with open('Input_UCs/{0}.txt'.format(file_name), 'r', encoding='utf-8') as uc_file:
+    input_path = "Input_UCs"
+    input_dataset_files = [x.replace('.txt', '') for x in listdir(input_path) if isfile(join(input_path, x)) and '.txt' in x]
+
+    for file_name in input_dataset_files:
+        output_file_name = 'Output_Pumls/{0}.puml'.format(file_name)
+
+        if file_exists(output_file_name):
+            continue
+
+        input_file_name = 'Input_UCs/{0}.txt'.format(file_name)
+
+        with open(input_file_name, 'r', encoding='utf-8') as uc_file:
             use_case = uc_file.read()
             yield file_name, use_case
 
@@ -13,5 +28,9 @@ def collect_puml(file_name, plant_uml_string, error_message):
         output_string = '[Error]' + error_message
         print('[UC2Sqd] Converting {0} failed, because: {1}'.format(file_name, error_message))
 
-    with open('Output_Pumls/{0}.puml'.format(file_name), 'w+', encoding='utf-8') as puml_file:
+    file_path = 'Output_Pumls/{0}.puml'.format(file_name)
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'w+', encoding='utf-8') as puml_file:
         puml_file.write(output_string)
+        puml_file.flush()
+        os.fsync(puml_file)
--- utilities.py
+++ utilities.py
@@ -1,7 +1,11 @@
+import os
+
+
 def readFilePlain(name):
     with open(name) as f:
         lines = f.readlines()
         return [l.strip()+'\n' for l in lines]
+
 
 def init_tokens():
     ret = []
@@ -10,3 +14,14 @@ def init_tokens():
         for token in lines:
             ret.append(token.strip())
     return ret
+
+
+def file_exists(file_name):
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            for _ in file:
+                break
+    except FileNotFoundError:
+        return False
+
+    return True
